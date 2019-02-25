9933d94e55305c996e1525701c3e399e4f0922db
Adam Czibere <adam.czibere@gmail.com>
2019-02-25 10:50:27 +0100

    rearranged scene

---
+++ RunnerPrototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject boulder;
+    public GameObject player;
+    public GameObject UI;
+
+    public void StartGame()
+    {
+        boulder.SetActive(true);
+        player.SetActive(true);
+        UI.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        boulder.SetActive(false);
+        player.SetActive(false);
+    }
+}
