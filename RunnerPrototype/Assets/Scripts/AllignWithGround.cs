e6f49cd7ba06438b706808b5c0cce366ab4a86eb
Adam Czibere <adam.czibere@gmail.com>
2019-02-25 12:02:23 +0100

    scene prepared

--- RunnerPrototype/Assets/Scripts/AllignWithGround.cs
+++ RunnerPrototype/Assets/Scripts/AllignWithGround.cs
@@ -9,23 +9,6 @@ public class AllignWithGround : MonoBehaviour
 
     private Vector3 dir = Vector3.forward;
 
-    // Update is called once per frame
-    //private void Update()
-    //{
-    //    RaycastHit hit;
-    //    // Does the ray intersect any objects excluding the player layer
-    //    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, rayLength))
-    //    {
-    //        if (hit.transform.gameObject.CompareTag("Ground"))
-    //        {
-    //            Debug.Log("Adjust rotation");
-    //            var toRotation = Quaternion.LookRotation(hit.transform.up);
-    //            //TODO: use different speed component
-    //            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.deltaTime);
-    //        }
-    //    }
-    //}
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Platform"))
@@ -38,7 +21,7 @@ public class AllignWithGround : MonoBehaviour
     {
         var toRotation = Quaternion.LookRotation(dir);
 
-        if (Quaternion.Angle(toRotation, transform.rotation) > 5f)
+        if (Quaternion.Angle(toRotation, transform.rotation) > 1f)
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.deltaTime);
             Debug.Log("Adjust rotation");
